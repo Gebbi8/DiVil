89f3a8537ca80435c8193d3a492f3a0ab8adc331
Tom Gebhardt <tom.gebhardt@uni-rostock.de>
2021-05-11 10:29:32 +0200

    bives log for dev

--- bives/bivesTool.js
+++ bives/bivesTool.js
@@ -1,6 +1,7 @@
 //Info-Content
 function getBivesData(v1, v2, callback){
 	var command = ["reportHtml", "reactionsSbgnJson", "xmlDiff", "separateAnnotations"];
+	//var command = ["reactionsSbgnJson"];
 	console.log(v1, v2);
 
 	var bivesJob = {
@@ -20,14 +21,13 @@ function getBivesData(v1, v2, callback){
 		"bivesJob=" + JSON.stringify (bivesJob),
 		function (data)
 		{
-			//console.log(data);
+			console.log(data);
 			//$("#bivesReport").html ($.parseJSON (data).reportHtml);
 			var sbgnJson = $.parseJSON (data).reactionsSbgnJson;
 			var comodiAnnotation = $.parseJSON(data).separateAnnotations;
 			//$("#sbgnJson").text(sbgnJson);
 			var xmlDiff = $.parseJSON (data).xmlDiff;
-			console.log(xmlDiff);
-			//console.log(xmlDocDiff, xmlDocSbml);
+			console.log(sbgnJson, xmlDiff, comodiAnnotation);
 			showSbgn(sbgnJson, xmlDiff, comodiAnnotation);
 			//console.log(sbgnJson);
 		}
